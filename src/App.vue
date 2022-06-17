ef003a4fa0180335adc0dba9e3b651d62eceabd0
liksleep <2103713963@qq.com>
2022-06-17 16:06:05 +0800

    main

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: liksleep 2103713963@qq.com
- * @Date: 2022-06-14 22:16:12
- * @LastEditors: liksleep 2103713963@qq.com
- * @LastEditTime: 2022-06-17 16:04:27
- * @FilePath: \vscode\vue\supermall\src\App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div id="app">
       
