d66ccdd3d3d2b081509c94b3b080f183de391062
liksleep <2103713963@qq.com>
2022-07-06 15:14:50 +0800

    mains

--- src/network/requst.js
+++ src/network/requst.js
@@ -1,17 +1,11 @@
-/*
- * @Author: liksleep 2103713963@qq.com
- * @Date: 2022-06-18 10:34:39
- * @LastEditors: liksleep 2103713963@qq.com
- * @LastEditTime: 2022-07-05 10:36:52
- * @FilePath: \vscode\vue\supermall\src\network\requst.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import axios from "axios";
 
 export function requst(config) {
     //1.创建axios实例
      const instancel = axios.create({
-         baseURL:'http://152.136.185.210:7878/api/hy66',  //'http://123.207.32.32:8000', 
+         baseURL:  
+         'http://152.136.185.210:7878/api/hy66',  
+        //  'http://123.207.32.32:8000', 
          timeout: 5000
      })
 
@@ -22,7 +16,7 @@ export function requst(config) {
             //拦截后必须返回(参数)
             return config
     },err => {
-            // console.log(err)
+        console.log(err)
 
     })
     // 响应拦截
---
+++ src/views/detail/chilcomptens/detailswiper.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: liksleep 2103713963@qq.com
+ * @Date: 2022-07-06 11:18:03
+ * @LastEditors: liksleep 2103713963@qq.com
+ * @LastEditTime: 2022-07-06 15:14:10
+ * @FilePath: \vscode\vue\supermall\src\views\detail\chilcomptens\detailswiper.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+    <div class="detail-swiper">
+        <swiper>
+            <swiperitem v-for="(item, index) in topImages" :key="index">
+                <img :src="item" alt="">
+            </swiperitem>
+        </swiper>
+    </div>
+</template>
+
+<script>
+import { swiper, swiperitem } from '@/components/common/swiper'
+
+export default {
+    name:'detailswiper',
+    components: {
+        swiper,
+        swiperitem
+    },
+    props:{
+        topImages: {
+            type: Array,
+            default() {
+                return []
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
