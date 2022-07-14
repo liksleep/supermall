21fe5ee169aa23746a5422d51bca770ad07d964a
liksleep <2103713963@qq.com>
2022-07-14 16:30:27 +0800

    main

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,11 +1,3 @@
-<!--
- * @Author: liksleep 2103713963@qq.com
- * @Date: 2022-07-14 15:07:24
- * @LastEditors: liksleep 2103713963@qq.com
- * @LastEditTime: 2022-07-14 16:29:44
- * @FilePath: \vscode\vue\supermall\src\views\detail\detail.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
     <div id="detail">
         <detailnavbar></detailnavbar>
