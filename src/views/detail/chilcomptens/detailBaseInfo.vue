962703217f64709ad32fefc21a3cb2679cb848c1
liksleep <2103713963@qq.com>
2022-07-15 11:10:56 +0800

    main

---
+++ src/views/detail/chilcomptens/detailBaseInfo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-if="Object.keys(goods).length !== 0" class="Base-info">
+        <div>{{goods.title}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'detailBaseInfo',
+    props: {
+        goods: {
+            type: Object,
+            default() {
+                return{}
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -2,20 +2,23 @@
     <div id="detail">
         <detailnavbar/>
         <detailswiper :topImages="topImages"/>
+        <detail-base-info :goods="goods"/>
     </div>
 </template>
 
 <script>
 import detailnavbar from './chilcomptens/detailnavbar'
 import detailswiper from './chilcomptens/detailswiper'
+import detailBaseInfo from './chilcomptens/detailBaseInfo'
 
-import {getDetail} from '@/network/detail'
+import {getDetail, Goods} from '@/network/detail'
 
 export default {
     name:'Detail',
     components: {
         detailnavbar,
-        detailswiper
+        detailswiper,
+        detailBaseInfo
 
 
     },
@@ -23,7 +26,8 @@ export default {
         return {
             iid: null,
             // res: null,
-            topImages: []
+            topImages: [],
+            goods:null
         }
     },
     activated() {
@@ -47,7 +51,13 @@ export default {
          getDetail(this.iid).then(res => {
             console.log(res);
 
-            this.topImages = res.result.itemInfo.topImages
+            // 1.获取轮播图数据
+            const data = res.result  
+            this.topImages = data.itemInfo.topImages
+
+            // 2.获取商品信息
+            this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
         })
     }
 }  
