65adbdb3bfd973219427c7b1cb50855315d4fd7d
liksleep <2103713963@qq.com>
2022-07-25 20:49:09 +0800

    main

--- src/views/detail/chilcomptens/detailCommentInfo.vue
+++ src/views/detail/chilcomptens/detailCommentInfo.vue
@@ -1,4 +1,4 @@
-<template>
+ <template>
     <div v-if="Object.keys(commentInfo).length !== 0" class="comment-info">
          <div class="info-header">
             <div class="header-title">用户评价</div>
